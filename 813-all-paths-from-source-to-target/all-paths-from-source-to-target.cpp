fa918b03c6fc56dec528f652a5c0fd8861efb610
Nainsi Motiyan <nainsi759@gmail.com>
2025-11-05 21:41:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (59.75%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void dfs(int node, vector<vector<int>>& graph, vector<int>& path, vector<vector<int>>& result) {
+        path.push_back(node);
+        if (node == graph.size() - 1) {
+            result.push_back(path);
+        } else {
+            for (auto next : graph[node]) {
+                dfs(next, graph, path, result);
+            }
+        }
+        path.pop_back();
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        vector<int> path;
+        dfs(0, graph, path, result);
+        return result;
+    }
+};
