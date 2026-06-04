cd351b760a8b50880c6283032a249169bc3794c2
Nainsi Motiyan <nainsi759@gmail.com>
2026-06-04 20:08:18 +0530

    Time: 44 ms (69.40%) | Memory: 9.3 MB (84.33%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.cpp
@@ -1,84 +1,18 @@
 class Solution {
 public:
-    struct Node {
-        long long cnt;
-        long long sum;
-    };
-
-    string s;
-    Node dp[20][2][3][11][11];
-    bool vis[20][2][3][11][11];
-
-    Node dfs(int pos, bool tight, bool started,
-             int lenState, int last2, int last1) {
-        
-        if (pos == (int)s.size()) {
-            return {1, 0};
-        }
-
-        if (!tight && vis[pos][started][lenState][last2][last1]) {
-            return dp[pos][started][lenState][last2][last1];
-        }
-
-        int limit = tight ? (s[pos] - '0') : 9;
-
-        Node ans{0, 0};
-
-        for (int d = 0; d <= limit; d++) {
-            bool ntight = tight && (d == limit);
-
-            if (!started && d == 0) {
-                Node child = dfs(pos + 1, ntight, false, 0, 10, 10);
-                ans.cnt += child.cnt;
-                ans.sum += child.sum;
-                continue;
-            }
-
-            if (!started) {
-                Node child = dfs(pos + 1, ntight, true, 1, 10, d);
-
-                ans.cnt += child.cnt;
-                ans.sum += child.sum;
-            } else {
-                long long add = 0;
-
-                if (lenState >= 2) {
-                    if ((last1 > last2 && last1 > d) ||
-                        (last1 < last2 && last1 < d)) {
-                        add = 1;
-                    }
-                }
-
-                int nextLenState = min(2, lenState + 1);
-                int nextLast2 = (lenState >= 1 ? last1 : 10);
-                int nextLast1 = d;
-
-                Node child = dfs(pos + 1, ntight, true,
-                                 nextLenState, nextLast2, nextLast1);
-
-                ans.cnt += child.cnt;
-                ans.sum += child.sum + child.cnt * add;
+    int totalWaviness(int num1, int num2) {
+        int cnt = 0;
+        for(int i = num1; i <= num2; i++){
+            string s = to_string(i);
+            if(s.size() < 3) continue;
+            for(int i = 1; i < s.size() - 1; i++){
+                int a = s[i-1] + '0';
+                int b = s[i] + '0';
+                int c = s[i+1] + '0';
+                if(b > a && b > c) cnt++;
+                else if(b < a && b < c) cnt++;
             }
         }
-
-        if (!tight) {
-            vis[pos][started][lenState][last2][last1] = true;
-            dp[pos][started][lenState][last2][last1] = ans;
-        }
-
-        return ans;
-    }
-
-    long long solve(long long n) {
-        if (n < 0) return 0;
-
-        s = to_string(n);
-        memset(vis, 0, sizeof(vis));
-
-        return dfs(0, true, false, 0, 10, 10).sum;
-    }
-
-    long long totalWaviness(long long num1, long long num2) {
-        return solve(num2) - solve(num1 - 1);
+        return cnt;
     }
 };
