cf573c2b8d9a48cd52ab32f8912d69b7ffc6bbe8
Nainsi Motiyan <nainsi759@gmail.com>
2026-06-19 20:42:07 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (44.68%) - LeetSync

---
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+
+        int altitude = 0;
+        int maxi = 0;
+
+        for (int x : gain) {
+            altitude += x;
+            maxi = max(maxi, altitude);
+        }
+
+        return maxi;
+    }
+};
